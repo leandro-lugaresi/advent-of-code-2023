cafed7052c6b262c1a4d832be487a2ec841b25e6
Leandro Lugaresi <leandrolugaresi92@gmail.com>
2023-12-08 18:48:09 -0300

    feat: add day1 part1

--- lib/advent_of_code/day_01.ex
+++ lib/advent_of_code/day_01.ex
@@ -1,7 +1,27 @@
 defmodule AdventOfCode.Day01 do
-  def part1(_args) do
+  def part1(args) do
+    args
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn line ->
+      line_to_calibration_value(line, &part1_finder/1)
+    end)
+    |> Enum.sum()
   end
 
   def part2(_args) do
+  end
+
+  defp line_to_calibration_value(line, finder_fn) do
+    line
+    |> String.trim()
+    |> String.replace(~r/[^0-9]/, "")
+    |> String.codepoints()
+    |> finder_fn.()
+  end
+
+  defp part1_finder(codepoints) do
+    [Enum.at(codepoints, 0), Enum.at(codepoints, -1)]
+    |> Enum.map(&String.to_integer/1)
+    |> Integer.undigits()
   end
 end
