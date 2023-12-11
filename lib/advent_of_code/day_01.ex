ecd3b91bf1d364b4a2e50996cb23c1f7fdf4ed50
Leandro Lugaresi <leandrolugaresi92@gmail.com>
2023-12-10 23:33:08 -0300

    feat: add part2 for day 1

--- lib/advent_of_code/day_01.ex
+++ lib/advent_of_code/day_01.ex
@@ -1,27 +1,117 @@
+defmodule SimpleParser do
+  defguardp is_digit(char) when char in ?0..?9
+
+  def next_number(<<char::utf8, rest::binary>>) when is_digit(char) do
+    {:ok, char - ?0, rest}
+  end
+
+  def next_number(<<_, rest::binary>>) do
+    next_number(rest)
+  end
+
+  def next_number(<<>>) do
+    {:error, :number_not_found}
+  end
+end
+
+defmodule AdvancedParser do
+  defguardp is_digit(char) when char in ?0..?9
+
+  def next_number(<<"one"::utf8, rest::binary>>) do
+    {:ok, 1, rest}
+  end
+
+  def next_number(<<"two"::utf8, rest::binary>>) do
+    {:ok, 2, rest}
+  end
+
+  def next_number(<<"three"::utf8, rest::binary>>) do
+    {:ok, 3, rest}
+  end
+
+  def next_number(<<"four"::utf8, rest::binary>>) do
+    {:ok, 4, rest}
+  end
+
+  def next_number(<<"five"::utf8, rest::binary>>) do
+    {:ok, 5, rest}
+  end
+
+  def next_number(<<"six"::utf8, rest::binary>>) do
+    {:ok, 6, rest}
+  end
+
+  def next_number(<<"seven"::utf8, rest::binary>>) do
+    {:ok, 7, rest}
+  end
+
+  def next_number(<<"eight"::utf8, rest::binary>>) do
+    {:ok, 8, rest}
+  end
+
+  def next_number(<<"nine"::utf8, rest::binary>>) do
+    {:ok, 9, rest}
+  end
+
+  def next_number(<<char::utf8, rest::binary>>) when is_digit(char) do
+    {:ok, char - ?0, rest}
+  end
+
+  def next_number(<<_, rest::binary>>) do
+    next_number(rest)
+  end
+
+  def next_number(<<>>) do
+    {:error, :number_not_found}
+  end
+end
+
 defmodule AdventOfCode.Day01 do
   def part1(args) do
-    args
+    solve(args, SimpleParser)
+  end
+
+  def part2(args) do
+    solve(args, AdvancedParser)
+  end
+
+  defp solve(input, parser) do
+    input
     |> String.split("\n", trim: true)
     |> Enum.map(fn line ->
-      line_to_calibration_value(line, &part1_finder/1)
+      [first_number(parser, line), last_number(parser, line)]
+      |> Integer.undigits()
     end)
     |> Enum.sum()
   end
 
-  def part2(_args) do
+  defp first_number(parser, line) do
+    case parser.next_number(line) do
+      {:ok, number, _rest} ->
+        number
+
+      {:error, rest} ->
+        first_number(parser, rest)
+    end
   end
 
-  defp line_to_calibration_value(line, finder_fn) do
-    line
-    |> String.trim()
-    |> String.replace(~r/[^0-9]/, "")
-    |> String.codepoints()
-    |> finder_fn.()
+  defp last_number(parser, line) do
+    case parser.next_number(line) do
+      {:ok, number, rest} ->
+        last_number(parser, rest, number)
+
+      {:error, _} ->
+        nil
+    end
   end
 
-  defp part1_finder(codepoints) do
-    [Enum.at(codepoints, 0), Enum.at(codepoints, -1)]
-    |> Enum.map(&String.to_integer/1)
-    |> Integer.undigits()
+  defp last_number(parser, line, number) do
+    case parser.next_number(line) do
+      {:ok, number, rest} ->
+        last_number(parser, rest, number)
+
+      {:error, _} ->
+        number
+    end
   end
 end
