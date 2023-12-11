bfbdfc33fcf0a54bcf0950229585004a28c91a4d
Leandro Lugaresi <leandrolugaresi92@gmail.com>
2023-12-11 00:02:40 -0300

    fix: handle "oneight" case
    I have to add the last char of the number to handle this case.
    Maybe we can improve that changing the function last_number

--- lib/advent_of_code/day_01.ex
+++ lib/advent_of_code/day_01.ex
@@ -17,53 +17,19 @@ end
 defmodule AdvancedParser do
   defguardp is_digit(char) when char in ?0..?9
 
-  def next_number(<<"one"::utf8, rest::binary>>) do
-    {:ok, 1, rest}
-  end
-
-  def next_number(<<"two"::utf8, rest::binary>>) do
-    {:ok, 2, rest}
-  end
-
-  def next_number(<<"three"::utf8, rest::binary>>) do
-    {:ok, 3, rest}
-  end
-
-  def next_number(<<"four"::utf8, rest::binary>>) do
-    {:ok, 4, rest}
-  end
-
-  def next_number(<<"five"::utf8, rest::binary>>) do
-    {:ok, 5, rest}
-  end
-
-  def next_number(<<"six"::utf8, rest::binary>>) do
-    {:ok, 6, rest}
-  end
-
-  def next_number(<<"seven"::utf8, rest::binary>>) do
-    {:ok, 7, rest}
-  end
-
-  def next_number(<<"eight"::utf8, rest::binary>>) do
-    {:ok, 8, rest}
-  end
-
-  def next_number(<<"nine"::utf8, rest::binary>>) do
-    {:ok, 9, rest}
-  end
-
-  def next_number(<<char::utf8, rest::binary>>) when is_digit(char) do
-    {:ok, char - ?0, rest}
-  end
-
-  def next_number(<<_, rest::binary>>) do
-    next_number(rest)
-  end
-
-  def next_number(<<>>) do
-    {:error, :number_not_found}
-  end
+  def next_number(<<char::utf8, rest::binary>>) when is_digit(char), do: {:ok, char - ?0, rest}
+  def next_number(<<"zero"::utf8, rest::binary>>), do: {:ok, 0, "o" <> rest}
+  def next_number(<<"one"::utf8, rest::binary>>), do: {:ok, 1, "e" <> rest}
+  def next_number(<<"two"::utf8, rest::binary>>), do: {:ok, 2, "o" <> rest}
+  def next_number(<<"three"::utf8, rest::binary>>), do: {:ok, 3, "e" <> rest}
+  def next_number(<<"four"::utf8, rest::binary>>), do: {:ok, 4, "r" <> rest}
+  def next_number(<<"five"::utf8, rest::binary>>), do: {:ok, 5, "e" <> rest}
+  def next_number(<<"six"::utf8, rest::binary>>), do: {:ok, 6, "x" <> rest}
+  def next_number(<<"seven"::utf8, rest::binary>>), do: {:ok, 7, "n" <> rest}
+  def next_number(<<"eight"::utf8, rest::binary>>), do: {:ok, 8, "t" <> rest}
+  def next_number(<<"nine"::utf8, rest::binary>>), do: {:ok, 9, "e" <> rest}
+  def next_number(<<_, rest::binary>>), do: next_number(rest)
+  def next_number(<<>>), do: {:error, :number_not_found}
 end
 
 defmodule AdventOfCode.Day01 do
